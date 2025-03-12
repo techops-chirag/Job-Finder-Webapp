4ee75226636b585d7c70761656eb3ca168f75ad5
techops-chirag <chirag111saini@gmail.com>
2025-03-12 17:27:32 +0530

    second commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -54,19 +54,19 @@ const Footer = () => {
         <div className='bg-purple-900'>
           <div className='container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row'>
             <p className='text-gray-300 text-sm text-center sm:text-left'>
-              &copy; 2023 JobFinder —
+              &copy; 2024 JobFinder —
               <a
-                href='https://www.linkedin.com/in/thatonekrish/'
+                href='https://www.linkedin.com/in/chiraggs/'
                 className='text-[#1199e7] ml-1'
                 target='_blank'
                 rel='noopener noreferrer'
               >
-                @ThatOneKrish
+                @techops-chirag
               </a>
             </p>
 
             <span className='sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-300 text-sm'>
-              Designed by ThatOneKrish
+              Designed by techops-chirag
             </span>
           </div>
         </div>
